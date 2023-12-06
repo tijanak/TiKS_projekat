dd864a9f2888b1bbf596f65b35cc3861fde3bc17
kv.tijana <kv.tijana@gmail.com>
2023-12-06 19:39:10 +0100

    prazni projekti

---
+++ End-to-endTestovi/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace End_to_endTestovi
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
