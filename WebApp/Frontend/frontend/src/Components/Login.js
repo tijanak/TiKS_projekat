7d17f78b84c40d8406b9ba98398c530757e012ff
kv.tijana <kv.tijana@gmail.com>
2024-01-21 00:15:47 +0100

    hmm

--- WebApp/Frontend/frontend/src/Components/Login.js
+++ WebApp/Frontend/frontend/src/Components/Login.js
@@ -3,48 +3,77 @@ import BACKEND from "../config";
 
 import CircularProgress from "@mui/material/CircularProgress";
 import Box from "@mui/material/Box";
-
+import Button from "@mui/material/Button";
+import { Input } from "@mui/base/Input";
+import Alert from "@mui/material/Alert";
+import { Stack } from "@mui/material";
+import FormLabel from "@mui/material/FormLabel";
+import Container from "@mui/material/Container";
+import { Typography } from "@mui/material";
 function Login({ setUserFunction }) {
   const [password, setPassword] = useState(null);
   const [username, setUserName] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
   return (
-    <div>
-      <p>Login page</p>
-      <label>Korisnicko ime:</label>
-      <input type="text" onChange={(t) => setUserName(t)}></input>
-      <label>Lozinka:</label>
-      <input type="password" onChange={(p) => setPassword(p)}></input>
-      <Box>
-        {loading ? (
-          <CircularProgress />
-        ) : (
-          <button
-            className="Button"
-            onClick={() => {
-              if (password == null) return;
-              if (username == null) return;
+    <Container>
+      <Stack>
+        <Typography variant="h2" component="h2">
+          Login page
+        </Typography>
+        <FormLabel>Korisnicko ime:</FormLabel>
+        <Input
+          type="text"
+          onChange={(t) => {
+            setError(false);
+            setUserName(t);
+          }}
+        ></Input>
+        <FormLabel>Lozinka:</FormLabel>
+        <Input
+          type="password"
+          onChange={(p) => {
+            setError(false);
+            setPassword(p);
+          }}
+        ></Input>
+        {error && <Alert severity="error">Pogresni podaci</Alert>}
+        <Box>
+          {loading ? (
+            <CircularProgress />
+          ) : (
+            <>
+              <Button
+                onClick={() => {
+                  if (password == null) return;
+                  if (username == null) return;
 
-              setLoading(true);
-              fetch(`${BACKEND}Korisnik/Login/` + username + "/" + "password")
-                .then((response) => {
-                  if (response.ok) {
-                    setUserFunction(response.json());
-                  } else {
-                    alert("pogresan login");
-                  }
-                })
-                .catch((error) => console.log(error))
-                .finally(() => {
-                  setLoading(false);
-                });
-            }}
-          >
-            login
-          </button>
-        )}
-      </Box>
-    </div>
+                  setLoading(true);
+                  fetch(
+                    `${BACKEND}Korisnik/Login/` + username + "/" + "password"
+                  )
+                    .then((response) => {
+                      if (response.ok) {
+                        setUserFunction(response.json());
+                      } else {
+                        setError(true);
+                      }
+                    })
+                    .catch((error) => console.log(error))
+                    .finally(() => {
+                      setLoading(false);
+                    });
+                }}
+                variant="contained"
+              >
+                Login
+              </Button>
+              <Button>Register</Button>
+            </>
+          )}
+        </Box>
+      </Stack>
+    </Container>
   );
 }
 
---
+++ WebApp/Frontend/frontend/src/Components/Register.js
@@ -0,0 +1,6 @@
+import React, { useState, useEffect } from "react";
+import BACKEND from "../config";
+function Register() {
+  return <div>register</div>;
+}
+export default Register;
