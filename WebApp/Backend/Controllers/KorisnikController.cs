43ac1dee8391ee2dfbcb27c346eb726107ce9a0c
jovana <119486693+vanavoj@users.noreply.github.com>
2024-01-20 22:34:03 +0100

    ispravljeni korisnik kontroler

--- WebApp/Backend/Controllers/KorisnikController.cs
+++ WebApp/Backend/Controllers/KorisnikController.cs
@@ -6,13 +6,9 @@ namespace Backend.Controllers;
 public class KorisnikController : ControllerBase
 {
     public ProjectContext Context { get; set; }
-    private SlucajController slucaj_controller;
-    private DonacijaController donacija_controller;
     public KorisnikController(ProjectContext Context)
     {
         this.Context = Context;
-        slucaj_controller = new SlucajController(Context);
-        donacija_controller = new DonacijaController(Context);
     }
 
     [HttpGet("preuzmikorisnika/{id}")]
@@ -46,7 +42,7 @@ public class KorisnikController : ControllerBase
     }
 
     [HttpPost("dodajkorisnika")]
-    public async Task<ActionResult> DodajKorisnika([FromBody] Korisnik korisnik, [FromQuery] int? id_donacije, [FromQuery] int? id_slucaja)
+    public async Task<ActionResult> DodajKorisnika([FromBody] Korisnik korisnik)
     {
         try
         {
@@ -58,20 +54,10 @@ public class KorisnikController : ControllerBase
             if (korisnik.Password == null || korisnik.Password == "") return BadRequest("Korisnik mora da ima sifru");
             if (korisnik.Password.Length > 50) return BadRequest("predugacka lozinka");
 
-            if (null == korisnik.Donacije) korisnik.Donacije = new List<Donacija>();
-            if (null == korisnik.Slucajevi) korisnik.Slucajevi = new List<Slucaj>();
+            korisnik.Donacije = new List<Donacija>();
+            korisnik.Slucajevi = new List<Slucaj>();
             Context.Korisnici.Add(korisnik);
             await Context.SaveChangesAsync();
-            if (null != id_donacije)
-            {
-                var don_actionResult = await DodeliDonaciju(korisnik.ID, (int)id_donacije);
-                if (don_actionResult.GetType() != typeof(OkObjectResult)) return BadRequest("Problemi sa dodelom donacije");
-            }
-            if (null != id_slucaja)
-            {
-                var sluc_actionResult = await DodeliSlucaj(korisnik.ID, (int)id_slucaja);
-                if (sluc_actionResult.GetType() != typeof(OkObjectResult)) return BadRequest("Problemi sa dodelom donacije");
-            }
             return Ok(korisnik.ID);
         }
         catch (Exception e)
@@ -80,137 +66,6 @@ public class KorisnikController : ControllerBase
         }
     }
 
-    [HttpPut("dodelislucaj")]
-    public async Task<ActionResult> DodeliSlucaj([FromQuery] int id_korisnika, [FromQuery] int id_slucaja)
-    {
-        try
-        {
-
-            var actionResult = PreuzmiKorisnika(id_korisnika);
-            if (actionResult.GetType() != typeof(OkObjectResult)) return NotFound("Nema takvog korisnika");
-            var actionResult_slucaj = await slucaj_controller.Preuzmi(id_slucaja);
-            if (actionResult_slucaj.GetType() != typeof(OkObjectResult)) return NotFound("Nema takvog slucaja");
-
-            var korisnik = ((OkObjectResult)actionResult).Value;
-            var slucaj = ((OkObjectResult)actionResult_slucaj).Value;
-            if (korisnik == null) return NotFound("Nema takvog korisnika");
-            if (slucaj == null) return NotFound("Nema takvog slucaja");
-
-            Korisnik k = (Korisnik)korisnik;
-            Slucaj s = (Slucaj)slucaj;
-            if (k.Slucajevi == null) k.Slucajevi = new List<Slucaj>();
-            for (int i = 0; i < k.Slucajevi.Count; i++)
-                if (k.Slucajevi[i].ID == id_slucaja) return BadRequest("Korisniku je vec dodeljen zeljeni slucaj");
-            k.Slucajevi.Add(s);
-            Context.Korisnici.Update(k);
-
-            s.Korisnik = k;
-            Context.Slucajevi.Update(s);
-
-            await Context.SaveChangesAsync();
-            return Ok(k);
-        }
-        catch (Exception e)
-        {
-            return BadRequest(e);
-        }
-    }
-
-    [HttpPut("dodelidonaciju")]
-    public async Task<ActionResult> DodeliDonaciju([FromQuery] int id_korisnika, [FromQuery] int id_donacije)
-    {
-        try
-        {
-            var actionResult = PreuzmiKorisnika(id_korisnika);
-            if (actionResult.GetType() != typeof(OkObjectResult)) return NotFound("Nema takvog korisnika");
-            var actionResult_donacija = await donacija_controller.Preuzmi(id_donacije);
-            if (actionResult_donacija.GetType() != typeof(OkObjectResult)) return NotFound("Nema takve donacije");
-
-
-            var korisnik = ((OkObjectResult)actionResult).Value;
-            var donacija = ((OkObjectResult)actionResult_donacija).Value;
-            if (korisnik == null) return NotFound("Nema takvog korisnika");
-            if (donacija == null) return NotFound("Nema takve donacije");
-
-            Korisnik k = (Korisnik)korisnik;
-            Donacija d = (Donacija)donacija;
-            if (k.Donacije == null) k.Donacije = new List<Donacija>();
-            for (int i = 0; i < k.Donacije.Count; i++)
-                if (k.Donacije[i].ID == id_donacije) return BadRequest("Korisniku je vec dodeljena zeljena donacija");
-            k.Donacije.Add(d);
-            Context.Korisnici.Update(k);
-
-            d.Korisnik = k;
-            Context.Donacije.Update(d);
-
-            await Context.SaveChangesAsync();
-            return Ok(k);
-        }
-        catch (Exception e)
-        {
-            return BadRequest(e);
-        }
-    }
-
-    [HttpPut("oduzmidonaciju")]
-    public async Task<ActionResult> OduzmiDonaciju([FromQuery] int id_korisnika, [FromQuery] int id_donacije)
-    {
-        try
-        {
-            var actionResult = PreuzmiKorisnika(id_korisnika);
-            if (actionResult.GetType() != typeof(OkObjectResult)) return NotFound("Nema takvog korisnika");
-
-            var korisnik = ((OkObjectResult)actionResult).Value;
-            if (korisnik == null) return NotFound("Nema takvog korisnika");
-            Korisnik k = (Korisnik)korisnik;
-            if (k.Donacije == null) return BadRequest("Korisnik nije donirao nista");
-            int i;
-            for (i = 0; i < k.Donacije.Count; i++)
-                if (k.Donacije[i].ID == id_donacije) break;
-            if (i == k.Donacije.Count) return BadRequest("Korisnik nije napravio tu donaciju");
-
-            k.Donacije.RemoveAt(i);
-            await donacija_controller.Obrisi(id_donacije);
-
-            Context.Korisnici.Update(k);
-            await Context.SaveChangesAsync();
-            return Ok(korisnik);
-        }
-        catch (Exception e)
-        {
-            return BadRequest(e);
-        }
-    }
-
-    [HttpPut("oduzmislucaj")]
-    public async Task<ActionResult> OduzmiSlucaj([FromQuery] int id_korisnika, [FromQuery] int id_slucaja)
-    {
-        try
-        {
-            var actionResult = PreuzmiKorisnika(id_korisnika);
-            if (actionResult.GetType() != typeof(OkObjectResult)) return NotFound("Nema takvog korisnika");
-
-            var korisnik = ((OkObjectResult)actionResult).Value;
-            if (korisnik == null) return NotFound("Nema takvog korisnika");
-            Korisnik k = (Korisnik)korisnik;
-            if (k.Slucajevi == null || k.Slucajevi.Count == 0) return BadRequest("Korisnik nema nijedan slucaj");
-            int i;
-            for (i = 0; i < k.Slucajevi.Count; i++)
-                if (k.Slucajevi[i].ID == id_slucaja) break;
-            if (i == k.Slucajevi.Count) return BadRequest("Ovaj slucaj nije korisnikov");
-
-            k.Slucajevi.RemoveAt(i);
-            await slucaj_controller.Obrisi(id_slucaja);
-
-            Context.Korisnici.Update(k);
-            await Context.SaveChangesAsync();
-            return Ok(korisnik);
-        }
-        catch (Exception e)
-        {
-            return BadRequest(e);
-        }
-    }
 
     [HttpPut("izmeniusernamepassword")]
     public async Task<ActionResult> IzmeniUsernamePassword([FromQuery] int id_korisnika, string? username, string? password)
