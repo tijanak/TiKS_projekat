6274667a09ebccf287b6641d5e6e44f41e1e353d
kv.tijana <kv.tijana@gmail.com>
2024-01-27 00:06:06 +0100

    wtf

--- WebApp/Frontend/frontend/src/Components/DodajNovost.js
+++ WebApp/Frontend/frontend/src/Components/DodajNovost.js
@@ -1,11 +1,18 @@
 import React, { useState, useEffect } from "react";
 import BACKEND from "../config";
-import { TextField, Box, Button, Typography, Card, CardActions, CardContent } from "@mui/material";
-import { FormControl, Input, FormHelperText, InputLabel } from '@mui/material';
+import {
+  TextField,
+  Box,
+  Button,
+  Typography,
+  Card,
+  CardActions,
+  CardContent,
+} from "@mui/material";
+import { FormControl, Input, FormHelperText, InputLabel } from "@mui/material";
 import { ImagePicker } from "react-file-picker";
 
-export default function DodajNovost(state){
-
+export default function DodajNovost(state) {
   const [novost, setNovost] = useState(null);
   const [datum, setDatum] = useState(null);
   const [tekst, setTekst] = useState("Unesi tekst");
@@ -13,93 +20,100 @@ export default function DodajNovost(state){
   const [e2, setE2] = useState(false);
   const [success, setSuccess] = useState("primary");
   const [slika, setSlika] = useState(null);
-  
-  const DodajNovost =async ()=>{
-    if(!novost || novost.length==0){
+
+  const DodajNovost = async () => {
+    if (!novost || novost.length == 0) {
       setTekst("Unesite tekst");
       setE(true);
     }
-    if(!datum){
+    if (!datum) {
       setE2(true);
     }
-    if(novost&&datum&&slika){
-    const n = {
-      "id": 0,
-      "tekst": novost,
-      "datum": datum,
-      "slika": slika,
-      "slucaj": {
-        "id": 0,
-        "naziv": "string",
-        "opis": "string",
-        "slike": [
-          "string"
-        ]
-      }
-    };
-    const requestOptions = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(n)
-    };
-      
-      fetch(`${BACKEND}Novost/dodajnovost?id_slucaja=${state.id_slucaja}`, requestOptions)
-      .then(response=>{if(response.ok) return response.json()})
-      .then(d=>{state.setLoading(!state.loading);setSuccess("success");console.log(d)})
-      .catch(e=>console.log(e));
-    }
-    }
+    if (novost && datum && slika) {
+      const n = {
+        id: 0,
+        tekst: novost,
+        datum: datum,
+        slika: slika,
+        slucaj: {
+          id: 0,
+          naziv: "string",
+          opis: "string",
+          slike: ["string"],
+        },
+      };
+      const requestOptions = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(n),
+      };
 
+      fetch(
+        `${BACKEND}Novost/dodajnovost?id_slucaja=${state.id_slucaja}`,
+        requestOptions
+      )
+        .then((response) => {
+          if (response.ok) return response.json();
+        })
+        .then((d) => {
+          state.setLoading(!state.loading);
+          setSuccess("success");
+          console.log(d);
+        })
+        .catch((e) => console.log(e));
+    }
+  };
 
-  const handleSetText = (event)=>{
+  const handleSetText = (event) => {
     setNovost(event.target.value);
-    if(e&&novost&&novost.length>0) {
+    if (e && novost && novost.length > 0) {
       setE(false);
       setTekst("Unesite tekst");
     }
-  }
+  };
 
-  const handleSetDate = (event)=>{
+  const handleSetDate = (event) => {
     setDatum(event.target.value);
-    if(e&&datum) {
+    if (e && datum) {
       setE2(false);
     }
-  }
-    return (<>
-    <FormControl>
-    <Typography variant="subtitle2" component="div">
-            Imaš novosti?
-          </Typography>
-      <TextField
-            id="outlined"
-            label={tekst}
-            multiline
-            maxRows={4}
-            inputProps={{ maxLength: 500 }}
-            onChange={handleSetText}
-            error={e}
-            color={success}
-          />
-          <Input
+  };
+  return (
+    <>
+      <FormControl>
+        <Typography variant="subtitle2" component="div">
+          Imaš novosti?
+        </Typography>
+        <TextField
+          id="outlined"
+          label={tekst}
+          multiline
+          maxRows={4}
+          inputProps={{ maxLength: 500 }}
+          onChange={handleSetText}
+          error={e}
+          color={success}
+        />
+        <Input
           variant="outlined"
           error={e2}
-        type="date"
-        slotProps={{
-          input: {
-            min: '2020-06-14',
-            max: new Date(),
-          },
-        }}
-        color={success}
-        onChange={handleSetDate}
-      />
-      <ImagePicker
+          type="date"
+          slotProps={{
+            input: {
+              min: "2020-06-14",
+              max: new Date(),
+            },
+          }}
+          color={success}
+          onChange={handleSetDate}
+        />
+        <ImagePicker
           extensions={["jpg", "jpeg", "png"]}
           dims={{
             minWidth: 0,
-            maxWidth: 2000,
+            maxWidth: 500,
             minHeight: 0,
-            maxHeight: 2000,
+            maxHeight: 500,
           }}
           onChange={(s) => {
             setSlika(s);
@@ -108,7 +122,10 @@ export default function DodajNovost(state){
         >
           <Button>Dodaj sliku</Button>
         </ImagePicker>
-      <Button variant="contained" onClick={()=>DodajNovost()} >Podeli novost</Button>
-    </FormControl>
-    </>);
+        <Button variant="contained" onClick={() => DodajNovost()}>
+          Podeli novost
+        </Button>
+      </FormControl>
+    </>
+  );
 }
